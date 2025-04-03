fcfdde67249921231beddb17f74009c8f1342edb
Carlos Malki <132589278+carlosmalki@users.noreply.github.com>
2025-04-03 23:27:20 +0200

    Ladde till vidare sidan som visas när man väljer en mattetyp

--- matematikspeleffekter.js
+++ matematikspeleffekter.js
@@ -120,37 +120,104 @@ function TaBortMeddelande() {
     document.getElementById("meddelande").innerHTML = ""; // Töm meddelandefältet
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    let playButton = document.getElementById("playButton");
+// Att gå fram
+
+function floatForward(containerfrom, containerto) {
     let loader = document.querySelector(".loader");
-    let container = document.getElementById("container");
-    let mathTypeContainer = document.getElementById("MathTypeContainer");
+    let containerFrom = document.getElementById(containerfrom);
+    let containerTo = document.getElementById(containerto);
 
-    playButton.addEventListener("click", function () {
-        // Visa loader och göm container
-        loader.style.display = "block";
-        container.classList.add("fade-out");
+    // Visa loader och göm container
+    loader.style.display = "block";
+    containerFrom.classList = "fade-out";
 
-        // Vänta tills fade-out animationen är klar innan vi gör något mer
-        container.addEventListener("animationend", function() {
-            setTimeout(() => {
-                // När loadern försvinner, visa MathTypeContainer
-                loader.style.display = "none";
-                container.style.display = "none";
-                mathTypeContainer.style.display = "block";
-                mathTypeContainer.classList.add("fade-in");
-            }, 100); // Vänta 2 sekunder så att loadern syns innan växlingen sker
-        }, { once: true });
-    });
-});
+    // Vänta tills fade-out animationen är klar innan vi gör något mer
+    containerFrom.addEventListener("animationend", function() {
+        setTimeout(() => {
+            // När loadern försvinner, visa MathTypeContainer
+            loader.style.display = "none";
+            containerFrom.style.display = "none";
+            containerTo.style.display = "block";
+            containerTo.classList = "fade-in";
+        }, 100); // Vänta 2 sekunder så att loadern syns innan växlingen sker
+    }, { once: true });
 
-
-function mathTypeText(id,typename) {
-    document.getElementById(id).innerHTML = typename;
 }
 
-function mathTypeSymbol(id,symbol) {
-    document.getElementById(id).innerHTML = symbol;
-    
-    
+// Att gå tillbaka
+
+function floatBack(containerfrom, containerto) {
+    let loader = document.querySelector(".loader");
+    let containerFrom = document.getElementById(containerfrom);
+    let containerTo = document.getElementById(containerto);
+
+    // Visa loader och göm container
+    loader.style.display = "block";
+    containerFrom.classList = "fade-out-1";
+
+    // Vänta tills fade-out animationen är klar innan vi gör något mer
+    containerFrom.addEventListener("animationend", function() {
+        setTimeout(() => {
+            // När loadern försvinner, visa MathTypeContainer
+            loader.style.display = "none";
+            containerFrom.style.display = "none";
+            containerTo.style.display = "block";
+            containerTo.classList = "fade-in-1";
+        }, 100); // Vänta 2 sekunder så att loadern syns innan växlingen sker
+    }, { once: true });
 }
+
+// att gå till mattetyp sidorna
+
+function floatToTypeSite(id,containerfrom, containerto) {
+
+    let loader = document.querySelector(".loader");
+    let containerFrom = document.getElementById(containerfrom);
+    let containerTo = document.getElementById(containerto);
+
+    loader.style.display = "block";
+    containerFrom.classList = "fade-out";
+
+    // Vänta tills fade-out animationen är klar innan vi gör något mer
+    containerFrom.addEventListener("animationend", function() {
+        setTimeout(() => {
+            // När loadern försvinner, visa MathTypeContainer
+            loader.style.display = "none";
+            containerFrom.style.display = "none";
+            containerTo.style.display = "block";
+            containerTo.classList = "fade-in";
+        }, 100); // Vänta 2 sekunder så att loadern syns innan växlingen sker
+    }, { once: true });
+
+    if (id == "addition") {
+        document.getElementById("typenameh1").innerHTML = "Addition";
+    }
+
+    else if (id == "multiplikation") {
+        document.getElementById("typenameh1").innerHTML = "Multiplication";
+    }
+
+    else if (id == "divition") {
+        document.getElementById("typenameh1").innerHTML = "Division";
+    }
+
+    else if (id == "subbtraktion") {
+        document.getElementById("typenameh1").innerHTML = "Subtraction";
+    }
+
+}
+
+
+function mathTypeText(id, typename) {
+    let element = document.getElementById(id);
+    element.innerHTML = typename;
+    element.style.fontSize = "30px"; 
+}
+
+function mathTypeSymbol(id, symbol) {
+    let element = document.getElementById(id);
+    element.innerHTML = symbol;
+    element.style.fontSize = "100px";
+}
+
+
